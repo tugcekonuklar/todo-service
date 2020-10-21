dece837d5498ec33260aa6525ab1122b92555e71
tugcekonuklar <tkonuklar@gmail.com>
2020-10-21 22:37:55 +0200

    feat: add swagger ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 }
 
 tasks.withType<Test> {
